1d770c325b41cd85ed98e8fd3ed0cbe7883b34a8
Jhoo17 <147905464+Jhoo17@users.noreply.github.com>
2023-11-17 12:49:19 +0800

    Delete php3 directory

--- php3/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "init.php";
-$id = $_GET['id'];
-
-$query_delete = "DELETE FROM mahasiswa WHERE id='$id'";
-mysqli_query($koneksi_ke_db, $query_delete);
-header("location: index.php");
-?>
--- php3/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hn = "localhost";
-$u = "root";
-$p = "";
-$db = "pemweb-d";
-// lakukan koneksi ke database
-$koneksi_ke_db = mysqli_connect($hn, $u, $p, $db);
-?>
